57a9b464cf885d6f8fb93d7ef10ecb2edc8e0a9e
Monali C G <cgmonali@gmail.com>
2023-10-19 18:46:45 +0530

    Adding new Movies into the list and showing it in the console

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { useState , useEffect} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
-
-
+import AddMovie from './components/AddMovie';
 function App() {
   const [movies,setMovies]=useState([]);
   const [isLoading,setIsLoading]=useState(false)
@@ -49,8 +48,17 @@ useEffect(() => {
       fetchMoviesHandler();
    
 }, [fetchMoviesHandler]);
+
+function addMovieHandler(movie) {
+  console.log(movie);
+}
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+
       <section>
         <button onClick={fetchMoviesHandler} >Fetch Movies</button>
       </section>
