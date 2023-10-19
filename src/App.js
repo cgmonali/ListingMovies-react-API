188ecf73933fc8658a965c48ed08221b631548f4
Monali C G <cgmonali@gmail.com>
2023-10-19 15:45:10 +0530

    handled error with try catch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useCallback } from 'react';
+import { useState , useEffect} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
@@ -8,10 +8,20 @@ import 'font-awesome/css/font-awesome.min.css';
 function App() {
   const [movies,setMovies]=useState([]);
   const [isLoading,setIsLoading]=useState(false)
- 
-async function fetchMoviesHandler(){
+  const [error,setError]=useState(null);
+  const [retrying, setRetrying] = useState(false);
+
+
+
+async function  fetchMoviesHandler(){
   setIsLoading(true);
-const response=await fetch('https://swapi.dev/api/films/');
+  setError(null)
+   try{
+const response=await fetch('https://swapi.dev/api/film/');
+
+if(!response.ok){
+  throw new Error('something went wrong.....retrying')
+}
 const data= await response.json();
 const transformedMovies = data.results.map((movieData) => {
   return  {//the flower bracket should be next to return on the same line not in the next line
@@ -25,7 +35,27 @@ const transformedMovies = data.results.map((movieData) => {
 });
 setMovies(transformedMovies); 
 setIsLoading(false);
-}
+}catch (error) {
+setError(error.message) ;
+setIsLoading(false);
+setRetrying(true);
+
+
+
+}};
+
+useEffect(() => {
+  if (retrying) {
+    const retryTimer = setTimeout(() => {
+      fetchMoviesHandler();
+    }, 5000);
+
+    return () => {
+      clearTimeout(retryTimer);
+      setRetrying(false);
+    };
+  }
+}, [retrying, fetchMoviesHandler]);
   return (
     <React.Fragment>
       <section>
@@ -33,7 +63,18 @@ setIsLoading(false);
       </section>
       <section>
        {!isLoading && <MoviesList movies={movies} />} 
-       {isLoading && <i className="fa fa-spinner fa-spin fa-3x"></i> } 
+       {!error&& isLoading  && <i className="fa fa-spinner fa-spin fa-3x"></i> } 
+      {!isLoading&& error &&
+      
+      <div>
+<p>{error}</p>
+
+        <button onClick={() => setRetrying(false)}>Stop Retrying</button>
+
+      </div>
+      
+      }
+      
       </section>
     </React.Fragment>
   );
