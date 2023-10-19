861967ddf0fd663e821753ad93b0c0e431de60b1
Monali C G <cgmonali@gmail.com>
2023-10-19 13:58:05 +0530

    obtained movie list by fetching through API call

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState } from 'react';
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies,setMovies]=useState([]);
+ 
+async function fetchMoviesHandler(){
+const response=await fetch('https://swapi.dev/api/films/');
+const data= await response.json();
+const transformedMovies = data.results.map((movieData) => {
+  return  {//the flower bracket should be next to return on the same line not in the next line
+    id: movieData.episode_id,
+    title: movieData.title,
+    openingText: movieData.opening_crawl,
+    releaseDate: movieData.release_date,
+  };
+
+});
+setMovies(transformedMovies);
+}
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler} >Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
